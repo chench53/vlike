dbba1762f904ef1ad9bdc0830bed2dbde64dbc45
tylr <tylr@MBP.local>
2022-04-24 09:44:03 -0700

    adding function headers

--- contracts/contracts/RatingSystem.sol
+++ contracts/contracts/RatingSystem.sol
@@ -2,22 +2,22 @@
 pragma solidity ^0.8;
 
 contract Rating {
-// functions needed
+    // functions needed
 
-function registerItem(string memory data) returns(uint256 itemId) {
+    function registerItem(string memory data) returns(uint256 itemId) {
 
-}
+    }
 
-function rate(uint 256 itemId, uint8 score) {
+    function rate(uint 256 itemId, uint8 score) {
 
-}
+    }
 
-function getRatingCount(uint256 itemId, uint8[] scores) returns (uint256[] scoreCount) {
+    function getRatingCount(uint256 itemId, uint8[] scores) returns (uint256[] scoreCount) {
 
-}
+    }
 
-function getRating(uint 256 itemId, address _user) returns(uint8 _rating) {
+    function getRating(uint 256 itemId, address _user) returns(uint8 _rating) {
 
-}
+    }
 
 }

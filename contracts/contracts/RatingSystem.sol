22b6870fdccc6f55ec212db2ae29cb5817048a49
tylr <tylr@MBP.local>
2022-04-24 09:42:24 -0700

    adding function headers

---
+++ contracts/contracts/RatingSystem.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract Rating {
+// functions needed
+
+function registerItem(string memory data) returns(uint256 itemId) {
+
+}
+
+function rate(uint 256 itemId, uint8 score) {
+
+}
+
+function getRatingCount(uint256 itemId, uint8[] scores) returns (uint256[] scoreCount) {
+
+}
+
+function getRating(uint 256 itemId, address _user) returns(uint8 _rating) {
+
+}
+
+}

c75a58a315bd34754e61270d54fa03422afab4e9
tylr <tylr@MBP.local>
2022-04-25 08:42:01 -0700

    updates complete

--- contracts/scripts/deploy.py
+++ contracts/scripts/deploy.py
@@ -9,7 +9,7 @@ def main():
     transaction = rating_contract.registerItem(bytes("www.youtube.com", 'utf-8'), {"from":account1})
     transaction.wait(1)
     print(rating_contract.itemIdCounter())
-    transaction = rating_contract.getRating(1, account1)
+    transaction = rating_contract.getRating(1, {"from": account1})
     transaction.wait(1)
 
     

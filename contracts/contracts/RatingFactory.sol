7f1784198c4af8521116a5b95908fc0f2e3528e7
Rohan <RohanZarathustra@gmail.com>
2022-04-30 16:01:49 -0400

    compiles!

--- contracts/contracts/RatingFactory.sol
+++ contracts/contracts/RatingFactory.sol
@@ -3,13 +3,23 @@
 pragma solidity 0.8.0;
 
 import "./RatingSystem.sol";
+import "./VlikeToken.sol";
 
-contract RatingFactory is Rating {
-
+contract RatingFactory {
     Rating[] public ratingArray;
+    VlikeToken public token;
+    bool public tokenEnabled;
+    bool public enableTokenAtInit;
+
+    function enableToken() public {
+        tokenEnabled = true;
+    }
 
     function createRatingSystemContract() public {
-        Rating rating = new Rating();
+        Rating rating = new Rating(token, enableTokenAtInit);
         ratingArray.push(rating);
+        if (enableTokenAtInit == true) {
+            enableToken();
+        }
     }
 }
